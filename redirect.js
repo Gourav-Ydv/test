070a82af2547fd0041e1aa088a6919291f5ad7c7
Gourav-Ydv <90626117+Gourav-Ydv@users.noreply.github.com>
2025-05-02 15:46:22 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,15 +1,7 @@
-// Delay to avoid instant redirect (blocks bots)
-setTimeout(function () {
-  // Obfuscation: Build the URL in parts
-  const part1 = "https://";
-  const part2 = "netex.";
-  const part3 = "jump2cloud.";
-  const part4 = "org";
-  const path = "/click?aid=226&oid=320&aff_sub2=a";
-
-  // Construct final URL
-  const finalURL = part1 + part2 + part3 + part4 + path;
-
-  // Redirect
-  window.location.href = finalURL;
-}, 700); // Delay helps bypass fast scanners
+// Some light obfuscation
+setTimeout(() => {
+  const domain = "netex.jump2cloud.org";
+  const path = "click?aid=226&oid=320&aff_sub2=a";
+  const url = `https://${domain}/${path}`;
+  window.location.href = url;
+}, 500);
